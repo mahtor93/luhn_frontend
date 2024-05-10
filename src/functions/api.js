c9f1e04e765116f1e12b41ee7f1b52407ba62455
user1 <mh.torneria@gmail.com>
2024-05-10 16:28:01 -0400

    edicion de URL

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL_BACK = "https://luhn-backend.onrender.com"
+const API_URL_BACK = "https://luhn-backend.onrender.com/"
 
 const apiGet = async (endpoint="") =>{
     try{
