6ee62a8739ed42e7921f2db7ec54f55390ecb234
user1 <mh.torneria@gmail.com>
2024-05-14 12:56:57 -0400

    creación del componente de tarjeta de contacto. Implementación en navbar/contact

---
+++ src/components/contact.js
@@ -0,0 +1,30 @@
+'use client'
+
+export default function ContactCard({ closeModal }) {
+
+    return (
+        <>
+            <div className="bg-slate-100 sm:h-[440px] rounded-lg sm:w-[512px]">
+                <h2 className="m-3 text-2xl text-slate-950">
+                    Let's Talk!
+                </h2>
+                <form className="flex flex-col">
+                    <input type="text" id="email" name="number" className="w-[300px] px-3 py-2 border rounded-md focus:outline-none focus:ring text-slate-950 focus:border-blue-300 m-3 shadow-lg" placeholder="Insert your Email" />
+                    <textarea className="px-3 py-2 m-3  placeholder-gray-400 text-gray-700 bg-white rounded-lg focus:outline-none focus:ring focus:border-blue-300 shadow-lg" rows="8" placeholder="Writte a Message"/>
+                    <div className="flex items-center justify-center">
+                        <button className="bg-blue-700 text-slate-200 px-4 py-2 rounded-md hover:bg-purple-700 focus:outline-none focus:bg-purple-700 m-3 shadow-lg">
+                            Send Message
+                        </button>
+
+                        <button className="bg-blue-700 text-slate-200 px-4 py-2 rounded-md hover:bg-purple-700 focus:outline-none focus:bg-purple-700 m-3 shadow-lg" onClick={closeModal}>
+                            Close
+                        </button>
+
+                    </div>
+
+                </form>
+            </div>
+        </>
+    )
+}
+
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,9 +3,13 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaCreditCard } from "react-icons/fa";
+import ContactCard from './contact';
+
 const Navbar = () => {
   const [openCloseMenu, setOpenCloseMenu] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
   const handleScroll = () => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop
     if (scrollTop === 0) {
@@ -14,9 +18,9 @@ const Navbar = () => {
       setIsScrolled(false)
     }
   }
-  
+
   const logotipo = '/images/wertmaster_logo.png' //logo del navbar
-  
+
   const bg_color_scroll_false = 'bg-gradient-to-b from-indigo-950 to-blue-800	' //color inicial del navbar
   const font_color_scroll_false = 'text-slate-200' //color de texto inicial del navbar
 
@@ -24,6 +28,10 @@ const Navbar = () => {
   const bg_blur_scroll_true = 'bg-black opacity-90' //fondo transparente, en caso de ser requerido
   const font_color_scroll_true = 'text-[#F8FAFC]' //color del texto al hacer scroll
 
+  const HandleContact = (e) => {
+    e.preventDefault();
+    setIsModalOpen(true);
+  }
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -41,84 +49,91 @@ const Navbar = () => {
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
-        const offset = 122; // Ajuste de desplazamiento (100px)
-        const elementPosition = element.offsetTop - offset;
-        window.scrollTo({
-            top: elementPosition,
-            behavior: "smooth"
-        });
+      const offset = 122; // Ajuste de desplazamiento (100px)
+      const elementPosition = element.offsetTop - offset;
+      window.scrollTo({
+        top: elementPosition,
+        behavior: "smooth"
+      });
     }
-};
+  };
 
   return (
-    <nav className={`transition duration-200 ease-in-out p-4 fixed top-0 w-full z-40 shadow-2xl
+    <>
+      <nav className={`transition duration-200 ease-in-out p-4 fixed top-0 w-full z-40 shadow-2xl
     ${!isScrolled ? `${bg_color_scroll_false} ${font_color_scroll_false}` : ""}
     ${isScrolled ? `${bg_blur_scroll_true} ${font_color_scroll_true}` : ""}`}>
-      <div className="max-w-7xl mx-auto sm:px-6">
-        <div className="flex items-center p-[16px] justify-between h-[90px] ">
-          <div className="flex-shrink-0">
-            <Link href="/" className="text-white font-bold">
-            <FaCreditCard className={`${isScrolled?font_color_scroll_true:font_color_scroll_false} text-4xl sm:text-7xl -rotate-12`} />
-            { /* <img src={logotipo} alt="Descripción de la imagen" />*/ }
-            </Link>
-          </div>
-          <div className="md:hidden block">
-            <GiHamburgerMenu className={`${!isScrolled?font_color_scroll_true:font_color_scroll_false} z-50 fixed top-10 right-10 size-7`} onClick={menu} />
-          </div>
-          <div className='hidden md:block'>
-            <div className="flex items-centerspace-x-[30px] md:ml-6">
-              <div className='space-x-4 p-[8px]'>
-                <a href="#home" className="nav-link mr-4 hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("home"), 100); }}>
-                  Home
-                </a>
-              </div>
-              <div className='space-x-4 p-[8px]'>
-                <a href="#utilities" className="nav-link mr-4 hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("utilities"), 100); }}>
-                  Utilities
-                </a>
-              </div>
+        <div className="max-w-7xl mx-auto sm:px-6">
+          <div className="flex items-center p-[16px] justify-between h-[90px] ">
+            <div className="flex-shrink-0">
+              <Link href="/" className="text-white font-bold">
+                <FaCreditCard className={`${isScrolled ? font_color_scroll_true : font_color_scroll_false} text-4xl sm:text-7xl -rotate-12`} />
+                { /* <img src={logotipo} alt="Descripción de la imagen" />*/}
+              </Link>
+            </div>
+            <div className="md:hidden block">
+              <GiHamburgerMenu className={`${!isScrolled ? font_color_scroll_true : font_color_scroll_false} z-50 fixed top-10 right-10 size-7`} onClick={menu} />
+            </div>
+            <div className='hidden md:block'>
+              <div className="flex items-centerspace-x-[30px] md:ml-6">
+                <div className='space-x-4 p-[8px]'>
+                  <a href="#home" className="nav-link mr-4 hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("home"), 100); }}>
+                    Home
+                  </a>
+                </div>
+                <div className='space-x-4 p-[8px]'>
+                  <a href="#utilities" className="nav-link mr-4 hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("utilities"), 100); }}>
+                    Utilities
+                  </a>
+                </div>
 
-              <div className='space-x-4 p-[8px]'>
-                <a href="#about" className="nav-link mr-4 hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("about"), 100); }}>
-                  About
-                </a>
+                <div className='space-x-4 p-[8px]'>
+                  <a href="#about" className="nav-link mr-4 hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("about"), 100); }}>
+                    About
+                  </a>
+                </div>
+                <div className='space-x-4 p-[8px]'>
+                  <a href="#contact" className="nav-link mr-4  hover:text-white text-sm font-medium" onClick={(e) => { HandleContact(e) }}>
+                    Contact
+                  </a>
+                </div>
+                {/* Agrega más enlaces según sea necesario */}
               </div>
-               <div className='space-x-4 p-[8px]'>
-                <a href="#contact" className="nav-link mr-4  hover:text-white text-sm font-medium" onClick={(e) => { e.preventDefault(); setTimeout(() => scrollToSection("contact"), 100); }}>
-                  Contact
-                </a>
-              </div>
-              {/* Agrega más enlaces según sea necesario */}
+            </div>
+            <div className={`sm:hidden ${!openCloseMenu ? "hidden" : ""}`} id="navbarResponsive">
+              <ul className={`navbar-nav ${!isScrolled ? "bg-blue-800" : "bg-black opacity-90"} text-slate-200 text-lg p-3 z-10 text-center mt-[115px] fixed w-full left-0 top-[5px] space-y-4`}>
+
+                <li className="nav-item ">
+                  <a href="#home" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("home"), 100); }}>
+                    Home
+                  </a>
+                </li>
+                <li className='nav-item '>
+                  <a href="#utilities" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("utilities"), 100); }}>
+                    Utilities
+                  </a>
+                </li>
+                <li className='nav-item '>
+                  <a href="#about" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("about"), 100); }}>
+                    About
+                  </a>
+                </li>
+                <li className='nav-item '>
+                  <a href="/servicios" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("contact"), 100); }}>
+                    Contact
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className={`sm:hidden ${!openCloseMenu?"hidden":""}`} id="navbarResponsive">
-            <ul className={`navbar-nav ${!isScrolled?"bg-blue-800":"bg-black opacity-90"} text-slate-200 text-lg p-3 z-10 text-center mt-[115px] fixed w-full left-0 top-[5px] space-y-4`}>
-             
-              <li className="nav-item ">
-              <a href="#home" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("home"), 100); }}>
-                  Home
-                </a>
-              </li>
-              <li className='nav-item '>
-              <a href="#utilities" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("utilities"), 100); }}>
-                  Utilities
-                </a>
-              </li>
-              <li className='nav-item '>
-              <a href="#about" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("about"), 100); }}>
-                  About
-                </a>
-              </li>
-              <li className='nav-item '>
-              <a href="/servicios" className="nav-link mr-4 hover:text-white font-medium" onClick={(e) => { e.preventDefault(); setOpenCloseMenu(!openCloseMenu); setTimeout(() => scrollToSection("contact"), 100); }}>
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {isModalOpen && ( 
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-gray-800 bg-opacity-75">
+            <ContactCard closeModal={() => setIsModalOpen(false)}/>
+          </div>    
+      )}
+    </>
   );
 }
 
